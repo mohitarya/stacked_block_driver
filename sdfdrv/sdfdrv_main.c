8d405dc1471672cad5d083a05d696516f00eafc2
root <root@mohit.localdomain>
2016-03-09 09:28:52 +0530

    Added basic function for device dsetup

--- sdfdrv/sdfdrv_main.c
+++ sdfdrv/sdfdrv_main.c
@@ -7,9 +7,34 @@
 
 unsigned int major_no = 0;
 char device_name[DEVICE_MAX_LEN];
+struct gendisk disk;
+
+setup_gendisk(struct gendisk)
+
+int setup_disk()
+{
+    int minors = 16;
+    int ret;
+    struct block_device bdev;
+    struct gendisk *gendisk;
+    //Look for the scsi block dev
+    bdev = lookup_bdev("/dev/sdb")
+    if (IS_ERR(bdev)){
+        ret = PTR_ERR(bdev)
+        printk("Lookup Bdev Return %d\n", ret);
+        return ERR_PTR(ret);
+    }
+    gendisk = alloc_disk(minors);
+    if (gendisk == NULL){
+        printk("Can not allocate gendisk");
+        return -1;
+    }
+    
+}
 
 static int __init sdfdrc_init(void)
 {
+    int ret_val = 0;
     memset(device_name, 0x0, DEVICE_MAX_LEN);
     strncpy(device_name, "mda", sizeof("mda"));
     major_no = reg_blkdev(major_no, device_name);
@@ -17,7 +42,9 @@ static int __init sdfdrc_init(void)
         printk("No major number is available Error: %d", major_no);
     }
     printk("Major No = %d\n", major_no);
-    return 0;
+
+    ret_val = setup_device(void);
+    return ret_val;
 }
 
 static void __exit sdfdrv_exit(void)
